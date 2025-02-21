bf97d7dd2472f1ed9d37d22640be7552f174774d
Oisin Minogue <oisinpminogue@gmail.com>
2025-02-21 18:03:46 +0000

    Trial UI

---
+++ src/main/java/org/example/eventdrivenproject/ClientGUIController_23390573_23381272.java
@@ -0,0 +1,14 @@
+package org.example.eventdrivenproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ClientGUIController_23390573_23381272 {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
