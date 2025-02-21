7450714c7b078982abfea997860c88ddf678eb55
lukescanlon <23390573@studentmail.ul.ie>
2025-02-21 14:24:18 +0000

    fix

--- Server_23390573_23381272.java
+++ Server_23390573_23381272.java
@@ -11,7 +11,6 @@ public class Server_23390573_23381272 {
     private static int clientConnections = 0;
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
         System.out.println("Opening port...\n");
 
         try{
