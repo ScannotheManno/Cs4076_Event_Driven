0612a35fc22a41a6cf6fcaea1633bb37669f8362
Oisin Minogue <84816052+M1n0j@users.noreply.github.com>
2025-02-24 16:25:38 +0000

    Delete src directory

--- src/main/java/IncorrectActionException.java
+++
@@ -1,9 +0,0 @@
-public class IncorrectActionException extends Exception {
-    public IncorrectActionException() {
-        super("Not a valid action");
-    }
-
-    public IncorrectActionException(String msg) {
-        super(msg);
-    }
-}
