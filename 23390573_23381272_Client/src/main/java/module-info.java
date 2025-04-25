983a53befc15af4b1804c8973e5f5c4ad76372c8
LukeySchan <150489309+ScannotheManno@users.noreply.github.com>
2025-04-25 12:36:44 +0100

    Clearing branch

--- 23390573_23381272_Client/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module org.openjfx._23381272_client {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens org.openjfx._23381272_client to javafx.fxml;
-    exports org.openjfx._23381272_client;
-}
